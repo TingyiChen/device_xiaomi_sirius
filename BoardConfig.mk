aacdbd5f3243fa4fcf85a8c1c8033b7a25b320ce
padarshr <padarshr@codeaurora.org>
2017-07-12 17:32:34 +0530

    sirius: Add device compatibility matrix.
    Add device compatibility matrix to common project and
    targets can point the build variable DEVICE_MATRIX_FILE to
    this file to build device compatibility matrix.
    
    Change-Id: If8e75c5ee8de0ef50d6937ffbd6453fb3f5eb648

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,6 +90,7 @@ TARGET_USES_HWC2 := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
