4094c170feb892294a9b7ef252cd496cbbed08c1
Isaac Chen <tingyi364@gmail.com>
2020-02-05 08:53:32 +0100

    sirius: Build custom biometrics service
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- sirius.mk
+++ sirius.mk
@@ -98,6 +98,10 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Biometrics
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sirius
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbthost_if
