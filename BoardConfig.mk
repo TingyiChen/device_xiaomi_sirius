d279eb6b1d06d03d6236b1e724ea4321c4d4a20d
Isaac Chen <tingyi364@gmail.com>
2020-02-18 02:33:35 +0100

    sirius: Switch SELinux to enforcing
    Change-Id: Idb6ac573128d8cbad407453d09716fa86a1787a3
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,6 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 androidboot.configfs=true firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 swiotlb=1
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
