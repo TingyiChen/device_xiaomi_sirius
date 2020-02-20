e09f8e89c06da447143a6f08b8ee404b230365ea
Isaac Chen <tingyi364@gmail.com>
2020-02-20 14:35:52 +0100

    sirius: Update audio config
     * Remove unused platform info XML
     * Reconfigure speaker&headphone related path
    
    Change-Id: I09e113e9e95efcf568d542e80f805425ee05d1d7
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- sirius.mk
+++ sirius.mk
@@ -91,7 +91,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
